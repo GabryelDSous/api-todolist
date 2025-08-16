c480340e8dbf49e4fdcc77bc61a0436fecf11209
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2025-08-16 09:55:53 -0300

    Entity TodoList criada e .properties configurado

---
+++ src/main/java/com/gabryel/API_TodoList/entity/TodoEntity.java
@@ -0,0 +1,75 @@
+package com.gabryel.API_TodoList.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "todolist")
+public class TodoEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nomeTarefa;
+	
+	@NotBlank
+	private String descricao;
+	
+	@Min(1)
+	@Max(10)
+	@NotBlank
+	private int prioridade;
+	
+	@NotBlank
+	private boolean realizado;
+
+	public TodoEntity() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeTarefa() {
+		return nomeTarefa;
+	}
+
+	public void setNomeTarefa(String nomeTarefa) {
+		this.nomeTarefa = nomeTarefa;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(int prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public boolean isRealizado() {
+		return realizado;
+	}
+
+	public void setRealizado(boolean realizado) {
+		this.realizado = realizado;
+	}
+}
